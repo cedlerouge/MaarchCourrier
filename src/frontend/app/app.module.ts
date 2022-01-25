a45f6c45811e3a251393bf056e0af7d99b12bf63
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2022-01-25 14:30:48 +0100

    FEAT #19517 TIME 0:35 WIP: add shipping modal

--- src/frontend/app/app.module.ts
+++ src/frontend/app/app.module.ts
@@ -157,6 +157,7 @@ import { AcknowledgementReceptionComponent } from './registeredMail/acknowledgem
 import { DatePipe } from '@angular/common';
 import { CheckSaeInterconnectionComponent } from './administration/parameter/other/checkSaeInterconnection/check-sae-interconnection.component';
 import { ContactSearchModalComponentComponent } from './administration/contact/page/form/contactSearchModal/contact-search-modal.component';
+import { ShippingModalComponent } from './sentResource/shippingModal/shipping-modal.component';
 
 registerLocaleData(localeFr, 'fr-FR');
 @Injectable()
@@ -304,6 +305,7 @@ export class MyHammerConfig extends HammerGestureConfig {
         CheckSaeInterconnectionComponent,
         ContactSearchModalComponentComponent,
         IndexingModelValuesSelectorComponent
+        ShippingModalComponent
     ],
     exports: [
         SharedModule
