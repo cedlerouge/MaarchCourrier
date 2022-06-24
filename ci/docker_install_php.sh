046b9431cf782720c13876d8c8e66d24f0f2349f
Jean-Laurent <jean-laurent.duzant@xelians.fr>
2022-06-24 19:52:13 +0200

    FIX #20486 TIME 0:05 clean up

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -10,7 +10,6 @@ apt-get install -y libpq-dev libxml2-dev libxslt1-dev libpng-dev unoconv xpdf-ut
 && docker-php-ext-configure pdo_pgsql --with-pdo-pgsql \
 && docker-php-ext-install pdo_pgsql pgsql \
 && docker-php-ext-install xsl \
-# && pecl install xdebug-2.9.3 \
 && pecl install xdebug-3.1.2 \
 && docker-php-ext-enable xdebug \
 && docker-php-ext-install gd \
