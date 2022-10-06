9f3974f3c09b58611e8117bd8081f97bfda83338
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2022-10-06 14:12:32 +0200

    FEAT #22361 TIME 3:50 create maarch and fast parapheur service + generalise send to external signatoryBook component

---
+++ src/frontend/service/externalSignatoryBook/maarch-parapheur.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FunctionsService } from '@service/functions.service';
+import { catchError, of, tap } from 'rxjs';
+import { NotificationService } from '@service/notification/notification.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MaarchParapheurService {
+
+    constructor(
+        public functions: FunctionsService,
+        private http: HttpClient,
+        private notify: NotificationService
+
+    ) { }
+
+    loadListModel(entityId: number) {
+        return new Promise((resolve) => {
+            this.http.get(`../rest/listTemplates/entities/${entityId}?type=visaCircuit&maarchParapheur=true`).pipe(
+                tap((data: any) => {
+                    resolve(data);
+                }),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    resolve(null);
+                    return of(false);
+                })
+            ).subscribe();
+        });
+    }
+
+    loadWorkflow(attachmentId: number, type: string) {
+        return new Promise((resolve) => {
+            this.http.get(`../rest/documents/${attachmentId}/maarchParapheurWorkflow?type=${type}`).pipe(
+                tap((data: any) => {
+                    resolve(data);
+                }),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    resolve(null);
+                    return of(false);
+                })
+            ).subscribe();
+        });
+    }
+
+    getUserAvatar(externalId: string) {
+        return new Promise((resolve) => {
+            this.http.get(`../rest/maarchParapheur/user/${externalId}/picture`).pipe(
+                tap((data: any) => {
+                    resolve(data.picture);
+                }),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    resolve(null);
+                    return of(false);
+                })
+            ).subscribe();
+        });
+    }
+
+    getOtpConfig() {
+        return new Promise((resolve) => {
+            this.http.get('../rest/maarchParapheurOtp').pipe(
+                tap((data: any) => {
+                    resolve(data.otp.length ?? null);
+                }),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    resolve(null);
+                    return of(false);
+                })
+            ).subscribe();
+        });
+    }
+}
