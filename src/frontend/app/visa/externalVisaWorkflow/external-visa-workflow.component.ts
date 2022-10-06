1010401701bc5f0865dbd214860174c600ba1c36
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2022-10-06 14:12:32 +0200

    FEAT #22361 TIME 3:50 create maarch and fast parapheur service + generalise send to external signatoryBook component

--- src/frontend/app/visa/externalVisaWorkflow/external-visa-workflow.component.ts
+++ src/frontend/app/visa/externalVisaWorkflow/external-visa-workflow.component.ts
@@ -26,6 +26,7 @@ export class ExternalVisaWorkflowComponent implements OnInit {
     @Input() injectDatas: any;
     @Input() adminMode: boolean;
     @Input() resId: number = null;
+    @Input() signatoryBookEnabled: 'maarchParapheur' | 'fastParapheur' = 'maarchParapheur';
 
     @Output() workflowUpdated = new EventEmitter<any>();
 
