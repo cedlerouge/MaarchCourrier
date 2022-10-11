cfcd6ef4845190d8ede903cdd0877d6384364bc5
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2022-10-11 12:08:05 +0200

    FEAT #22432 TIME 2 create external signatory book generator service

--- src/frontend/app/visa/externalVisaWorkflow/external-visa-workflow.component.ts
+++ src/frontend/app/visa/externalVisaWorkflow/external-visa-workflow.component.ts
@@ -25,7 +25,6 @@ export class ExternalVisaWorkflowComponent implements OnInit {
     @Input() injectDatas: any;
     @Input() adminMode: boolean;
     @Input() resId: number = null;
-    @Input() signatoryBookEnabled: 'maarchParapheur' | 'fastParapheur' = 'maarchParapheur';
 
     @Output() workflowUpdated = new EventEmitter<any>();
 
