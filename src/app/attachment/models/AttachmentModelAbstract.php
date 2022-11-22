d4d3b1bcdd9dc23e5eba3dba6b383e610db0e2b4
Jean-Laurent <jean-laurent.duzant@xelians.fr>
2022-11-22 12:23:37 +0100

    FIX #17635 TIME 0:05 update AttachmentModel::update

--- src/app/attachment/models/AttachmentModelAbstract.php
+++ src/app/attachment/models/AttachmentModelAbstract.php
@@ -80,12 +80,12 @@ abstract class AttachmentModelAbstract
     public static function update(array $args)
     {
         ValidatorModel::notEmpty($args, ['where', 'data']);
-        ValidatorModel::arrayType($args, ['set', 'where', 'data', 'postSet']);
+        ValidatorModel::arrayType($args, ['set', 'postSet', 'where', 'data']);
 
         DatabaseModel::update([
             'table'   => 'res_attachments',
             'set'     => $args['set'] ?? [],
-            'postSet' => !empty($args['postSet']) ? $args['postSet'] : [],
+            'postSet' => $args['postSet'] ?? [],
             'where'   => $args['where'],
             'data'    => $args['data']
         ]);
