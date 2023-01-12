a844c200fa99019a14546bcae5f48eeb275c1a24
Alex ORLUC <alex.orluc@maarch.org>
2023-01-12 15:55:51 +0100

    FIX #23456 TIME 0:25 add log on setasign errors + add function get setasign lib

--- src/app/resource/controllers/ResController.php
+++ src/app/resource/controllers/ResController.php
@@ -842,16 +842,29 @@ class ResController extends ResourceControlController
         $pathToPdf = $docserver['path_template'] . $adrPdf[0]['path'] . $adrPdf[0]['filename'];
         $pathToPdf = str_replace('#', '/', $pathToPdf);
 
-        $libDir = CoreConfigModel::getLibrariesDirectory();
-        if (!empty($libDir) && is_file($libDir . 'SetaPDF-FormFiller-Full/library/SetaPDF/Autoload.php')) {
-            require_once($libDir . 'SetaPDF-FormFiller-Full/library/SetaPDF/Autoload.php');
+        $libPath = CoreConfigModel::getSetaSignFormFillerLibrary();
+        if (!empty($libPath)) {
+            require_once($libPath);
 
             $document = \SetaPDF_Core_Document::loadByFilename($pathToPdf);
             $pages = $document->getCatalog()->getPages();
             $pageCount = count($pages);
         } else {
-            $pdf = new Fpdi('P', 'pt');
-            $pageCount = $pdf->setSourceFile($pathToPdf);
+            try {
+                $pdf = new Fpdi('P', 'pt');
+                $pageCount = $pdf->setSourceFile($pathToPdf);
+            } catch (\Exception $e) {
+                LogsController::add([
+                    'isTech'    => true,
+                    'moduleId'  => 'resources',
+                    'level'     => 'ERROR',
+                    'tableName' => 'res_letterbox',
+                    'recordId'  => $args['resId'],
+                    'eventType' => 'thumbnail',
+                    'eventId'   => $e->getMessage()
+                ]);
+                return $response->withStatus(400)->withJson(['errors' => $e->getMessage()]);
+            }
         }
 
         return $response->withJson(['fileContent' => $base64Content, 'pageCount' => $pageCount]);
