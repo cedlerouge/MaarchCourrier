cd3612cc687294d7e50eb8b240fc3f56e92248d9
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2023-04-27 11:44:35 +0200

    FEAT #21020 0 update migration version

--- migration/2301.0.4.sql
+++ migration/2301.0.4.sql
@@ -1,7 +1,7 @@
 -- *************************************************************************--
 --                                                                          --
 --                                                                          --
--- Model migration script - 2301.0.3 to 2301.0.4                            --
+-- Model migration script - 2301.0.4 to 2301.1.0                            --
 --                                                                          --
 --                                                                          --
 -- *************************************************************************--
@@ -10,4 +10,4 @@
 UPDATE usergroups_services SET service_id = 'update_delete_attachments' WHERE service_id = 'manage_attachments';
 
 
-UPDATE parameters SET param_value_string = '2301.0.4' WHERE id = 'database_version';
+UPDATE parameters SET param_value_string = '2301.1.0' WHERE id = 'database_version';
