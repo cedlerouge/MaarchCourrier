37bf1dab7c66e622bb94825a7fb1dffca0fd2ffa
Joseph AKEL <joseph.akel@maarch.org>
2023-07-05 11:41:43 +0200

    FEAT #25684 02:15 Fix 'if' condition in ngOnInit for account-link.component.ts

--- src/frontend/app/administration/user/user-administration.component.ts
+++ src/frontend/app/administration/user/user-administration.component.ts
@@ -261,7 +261,6 @@ export class UserAdministrationComponent implements OnInit {
                 } else {
                     this.createExternalSignatoryBookAccount(result, result.login);
                 }
-
             }
         });
     }
