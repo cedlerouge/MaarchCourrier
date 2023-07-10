105f7c7cc07c189bc08bf7ffbf13b48a621f9c37
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2023-07-10 18:37:29 +0200

    FIX #25845 TIME 0 fix unit test

--- test/unitTests/app/basket/BasketControllerTest.php
+++ test/unitTests/app/basket/BasketControllerTest.php
@@ -282,9 +282,9 @@ class BasketControllerTest extends CourrierTestCase
                 $this->assertSame('PSF', $groupAction->redirects[0]->entity_id);
                 $this->assertSame('', $groupAction->redirects[0]->keyword);
                 $this->assertSame('USERS', $groupAction->redirects[0]->redirect_mode);
-                $this->assertSame('PSF', $groupAction->redirects[1]->entity_id);
+                $this->assertSame('PSO', $groupAction->redirects[1]->entity_id);
                 $this->assertSame('', $groupAction->redirects[1]->keyword);
-                $this->assertSame('USERS', $groupAction->redirects[1]->redirect_mode);
+                $this->assertSame('ENTITY', $groupAction->redirects[1]->redirect_mode);
             }
         }
     }
