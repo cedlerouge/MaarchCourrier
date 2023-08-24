af2b081b56914a97361e30484672487826fba1a2
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2023-08-24 16:34:46 +0200

    FEAT #26239 TIME 0:35 add migration guard

---
+++ src/frontend/service/migration.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { MigrationService } from './migration.service';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MigrationCheckGuard implements CanActivate {
+    constructor(
+        private migrationService: MigrationService
+    ) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+        if (this.migrationService.isMigrating) {
+            this.migrationService.logoutAndShowAlert();
+        }
+        return of(true);
+    }
+}
