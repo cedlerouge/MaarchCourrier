7498070648e183eb396d0e7cf75c8174920a625c
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2023-09-05 15:37:45 +0200

    FEAT #26241 TIME 0 change description for functions in AutoUpdateInterface

--- src/core/interfaces/AutoUpdateInterface.php
+++ src/core/interfaces/AutoUpdateInterface.php
@@ -18,20 +18,29 @@ namespace SrcCore\interfaces;
 Interface AutoUpdateInterface
 {
     /**
-     * Function to perform any backups for the update
-     * @return  mixed   true if backup is sucessful or array -> ['errors' => any] if the backup failed
+     * Function to perform any backups for the update.
+     * Use trycatch at the root of the function.
+     * 
+     * @return  mixed   true if backup is sucessful
+     * @throws  \Exception if the backup failed
      */
     public static function backup();
 
     /**
-     * Function to perform any update for a feature
-     * @return  mixed   true if update is sucessful or array -> ['errors' => any] if the update failed
+     * Function to perform any update for a feature.
+     * Use trycatch at the root of the function.
+     * 
+     * @return  mixed   true if update is sucessful
+     * @throws  \Exception if the update failed
      */
     public static function update();
 
     /**
-     * Function to perform any rollback for the update
-     * @return  mixed   true if rollback is sucessful or array -> ['errors' => any] if the rollback failed
+     * Function to perform any rollback for the update.
+     * Use trycatch at the root of the function.
+     * 
+     * @return  mixed   true if rollback is sucessful
+     * @throws  \Exception if the rollback failed
      */
     public static function rollback();
 }
