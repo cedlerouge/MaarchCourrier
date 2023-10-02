08ce0a42cf7ea46b418ab6ed6993b02c03b05abd
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2023-10-02 15:00:06 +0200

    FEAT #23221 TIME 0:25 rm unused lines + improve check

--- src/app/resource/controllers/ResController.php
+++ src/app/resource/controllers/ResController.php
@@ -1319,33 +1319,22 @@ class ResController extends ResourceControlController
             }
         } else {
             if (!empty($body['destination'])) {
-                $entity = EntityModel::getById(['select' => ['entity_id'], 'id' => $body['destination']]);
-                if (empty($entity)) {
-                    return $response->withStatus(400)->withJson(['errors' => 'Entity does not exist']);
-                }
                 $where = ['entity_id = ? AND type = ?'];
                 $data = [$body['destination'], 'diffusionList'];
-                $listTemplates = ListTemplateModel::get(['select' => ['*'], 'where' => $where, 'data' => $data]);
-                foreach ($listTemplates as $key => $listTemplate) {
-                    $listTemplateItems = ListTemplateItemModel::get(['select' => ['*'], 'where' => ['list_template_id = ?'], 'data' => [$listTemplate['id']]]);
-                    foreach ($listTemplateItems as $itemKey => $value) {
-                        if ($value['item_type'] == 'entity') {
-                            $listTemplateItems[$itemKey]['labelToDisplay'] = EntityModel::getById(['id' => $value['item_id'], 'select' => ['entity_label']])['entity_label'];
-                            $listTemplateItems[$itemKey]['descriptionToDisplay'] = '';
-                        } else {
-                            $user = UserModel::getById(['id' => $value['item_id'], 'select' => ['firstname', 'lastname', 'external_id', 'status']]);
-                            if (empty($user) || in_array($user['status'], ['SPD', 'DEL'])) {
-                                if ($listTemplate['type'] == 'diffusionList') {
-                                    unset($listTemplateItems[$itemKey]);
-                                    continue;
-                                }
+                $listTemplate = ListTemplateModel::get(['select' => ['*'], 'where' => $where, 'data' => $data])[0];
+                $listTemplateItems = ListTemplateItemModel::get(['select' => ['*'], 'where' => ['list_template_id = ?'], 'data' => [$listTemplate['id']]]);
+                foreach ($listTemplateItems as $itemKey => $value) {
+                    if ($value['item_type'] == 'user') {
+                        $user = UserModel::getById(['id' => $value['item_id'], 'select' => ['firstname', 'lastname', 'external_id', 'status']]);
+                        if (empty($user) || in_array($user['status'], ['SPD', 'DEL'])) {
+                            if ($listTemplate['type'] == 'diffusionList') {
+                                unset($listTemplateItems[$itemKey]);
+                                continue;
                             }
-                            $listTemplateItems[$itemKey]['labelToDisplay'] = "{$user['firstname']} {$user['lastname']}";
-                            $listTemplateItems[$itemKey]['descriptionToDisplay'] = UserModel::getPrimaryEntityById(['id' => $value['item_id'], 'select' => ['entity_label']])['entity_label'];
                         }
                     }
-                    $listTemplates[$key]['items'] = array_values($listTemplateItems);
                 }
+                $listTemplate['items'] = array_values($listTemplateItems);
                 if (!empty($listTemplateItems)) {
                     foreach ($listTemplateItems as $diffusion) {
                         if ($diffusion['item_mode'] == 'dest') {
