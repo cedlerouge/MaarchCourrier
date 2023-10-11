6abffe7a9e98e0328a8a4088a09865b6d0da04c4
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2023-10-11 12:01:56 +0200

    FIX #26830 TIME 0 add database backup tables

--- migration/2301.1.5.sql
+++ migration/2301.1.5.sql
@@ -5,18 +5,18 @@
 --                                                                          --
 --                                                                          --
 -- *************************************************************************--
+--DATABASE_BACKUP|users|actions|parameters
 
 UPDATE users SET preferences = preferences - 'outlookPassword' WHERE preferences->>'outlookPassword' IS NOT NULL;
 
 UPDATE actions
 SET parameters = CASE
-                     WHEN
-                         (SELECT param_value_int FROM parameters where id = 'keepDestForRedirection') != '0' THEN
-                         parameters::jsonb ||
-                             '{"keepCopyForRedirection": false, "keepDestForRedirection:": true, "keepOtherRoleForRedirection": false}'
-                     WHEN (SELECT param_value_int FROM parameters where id = 'keepDestForRedirection') = '0' THEN
-                         parameters::jsonb ||
-                             '{"keepCopyForRedirection": false, "keepDestForRedirection:": false, "keepOtherRoleForRedirection": false}' END
+                    WHEN
+                        (SELECT param_value_int FROM parameters where id = 'keepDestForRedirection') != '0' THEN
+                            parameters::jsonb || '{"keepCopyForRedirection": false, "keepDestForRedirection:": true, "keepOtherRoleForRedirection": false}'
+                    WHEN 
+                        (SELECT param_value_int FROM parameters where id = 'keepDestForRedirection') = '0' THEN
+                            parameters::jsonb || '{"keepCopyForRedirection": false, "keepDestForRedirection:": false, "keepOtherRoleForRedirection": false}' END
 WHERE component = 'redirectAction';
 
 DELETE FROM parameters WHERE id = 'keepDestForRedirection';
