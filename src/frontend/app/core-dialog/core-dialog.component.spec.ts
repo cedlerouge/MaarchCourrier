154b10621eb6faa152c8841e366768a3be73f837
Alex ORLUC <alex.orluc@maarch.org>
2023-10-30 20:36:31 +0100

    FEAT #27003 TIME 0:30 fix TU

--- src/frontend/app/core-dialog/core-dialog.component.spec.ts
+++ src/frontend/app/core-dialog/core-dialog.component.spec.ts
@@ -1,58 +1,55 @@
-import { ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
-import { TranslateService, TranslateStore } from '@ngx-translate/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppSharedModule } from 'src/app/shared/app-shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CoreDialogComponent } from './core-dialog.component';
-import { EntityService } from 'src/services/entity.service';
 import { MatDialogRef } from '@angular/material/dialog';
-import { AppService } from 'src/services/app.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ParameterService } from '../shared/services/parameter.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AppService } from '@service/app.service';
+import { AuthService } from '@service/auth.service';
+import { HeaderService } from '@service/header.service';
+import { TranslateService, TranslateStore } from '@ngx-translate/core';
+import { InternationalizationModule } from '@service/translate/internationalization.module';
+import { FoldersService } from '@appRoot/folder/folders.service';
+import { AppMaterialModule } from '@appRoot/app-material.module';
+import { NotificationService } from '@service/notification/notification.service';
+import { PrivilegeService } from '@service/privileges.service';
+import { LatinisePipe } from 'ngx-pipes';
+import { DatePipe } from '@angular/common';
+import { AdministrationService } from '@appRoot/administration/administration.service';
 
 describe('CoreDialogComponent', () => {
     let component: CoreDialogComponent;
     let fixture: ComponentFixture<CoreDialogComponent>;
-    let appService: AppService;
-    let httpCtl: HttpTestingController;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [AppSharedModule, RouterTestingModule, BrowserAnimationsModule, HttpClientTestingModule],
+            imports: [InternationalizationModule, AppMaterialModule, RouterTestingModule, BrowserAnimationsModule, HttpClientTestingModule],
             providers: [
+                AuthService,
+                HeaderService,
                 TranslateService,
                 TranslateStore,
                 AppService,
-                EntityService,
-                { provide: ParameterService, useValue: {} },
+                FoldersService,
+                NotificationService,
+                PrivilegeService,
+                LatinisePipe,
+                DatePipe,
+                AdministrationService,
                 { provide: MatDialogRef, useValue: {} }
             ],
             declarations: [CoreDialogComponent]
         }).compileComponents();
-        httpCtl = TestBed.inject(HttpTestingController);
     });
 
     beforeEach(() => {
-        appService = TestBed.inject(AppService);
-        appService.apiURL = 'http://localhost';
         fixture = TestBed.createComponent(CoreDialogComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it('should create', () => {
+    fit('should create', () => {
         expect(component).toBeTruthy();
-    });
-
-    describe('init config', () => {
-        it('get config', fakeAsync(() => {
-            httpCtl.expectOne('config/config.json');
-            flush();
-        }));
-        it('get custom lang', fakeAsync(() => {
-            httpCtl.expectOne('config/fr_custom.json');
-            flush();
-        }));
     });
 });
