4f0d5705069d5b26855be14ddab6519c79010d37
Joseph AKEL <joseph.akel@maarch.org>
2023-11-02 18:08:56 +0100

    FEAT #26804 TIME 00:05 Reformating code + adding copyright

--- test/unitTests/app/external/Pastell/Mock/SendToPastellSpy.php
+++ test/unitTests/app/external/Pastell/Mock/SendToPastellSpy.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
 declare(strict_types=1);
 
 namespace MaarchCourrier\Tests\app\external\Pastell\Mock;
