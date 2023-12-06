014337315fe0b58effdfdbec9665f05050664004
Joseph AKEL <joseph.akel@maarch.org>
2023-12-06 10:23:39 +0100

    FEAT #26916 TIME 00:40 Adding config param for unitTests

--- test/unitTests/define.php
+++ test/unitTests/define.php
@@ -1,21 +1,27 @@
 <?php
 
 /**
-* Copyright Maarch since 2008 under licence GPLv3.
-* See LICENCE.txt file at the root folder for more details.
-* This file is part of Maarch software.
-*
-*/
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+use SrcCore\models\CoreConfigModel;
+use User\models\UserModel;
 
 require_once 'vendor/autoload.php';
 
 $GLOBALS['login'] = 'superadmin';
-$userInfo = \User\models\UserModel::getByLogin(['login' => $GLOBALS['login'], 'select' => ['id']]);
+$userInfo = UserModel::getByLogin(['login' => $GLOBALS['login'], 'select' => ['id']]);
 $GLOBALS['id'] = $userInfo['id'];
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 
-date_default_timezone_set(\SrcCore\models\CoreConfigModel::getTimezone());
+date_default_timezone_set(CoreConfigModel::getTimezone());
 
-$language = \SrcCore\models\CoreConfigModel::getLanguage();
+$language = CoreConfigModel::getLanguage();
 require_once("src/core/lang/lang-{$language}.php");
 
+$config = file_get_contents('config/config.json.default');
+$config = json_decode($config, true);
+$config['config']['newInternalParaph'] = true;
