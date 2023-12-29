8831ad75596bcb826a0c21330ee43254e82ad3c5
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2023-12-29 14:55:46 +0100

    FEAT #27405 TIME 0:05 check if docserver encryption lock is active or not before decrypting file content

--- src/app/resource/Infrastructure/ResourceFile.php
+++ src/app/resource/Infrastructure/ResourceFile.php
@@ -83,7 +83,7 @@ class ResourceFile implements ResourceFileInterface
             return null;
         }
 
-        if ($isEncrypted) {
+        if (!CoreConfigModel::isDocserverEncryptionLocked() && $isEncrypted) {
             $fileContent = PasswordController::decrypt(['encryptedData' => base64_encode($fileContent)]);
         }
 
