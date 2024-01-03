608edcd0098467912574586c9b6fe2188add6381
ncoutur <couture@docimsol.com>
2024-01-03 16:39:05 +0100

    FEAT #27248 TIME 00:05 Add parameter in migration file

--- migration/2301.3.0/2301.3.0.sql
+++ migration/2301.3.0/2301.3.0.sql
@@ -9,9 +9,11 @@
 
 -- Docserver encryption
 -- Checks if the "is_encrypted" column exists in the "docservers" table, if it doesn't exist, add the column.
-DO $$ 
+DO $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'docservers' AND column_name = 'is_encrypted') THEN
         ALTER TABLE docservers ADD COLUMN is_encrypted BOOL NOT NULL DEFAULT FALSE;
     END IF;
 END $$;
+
+INSERT INTO parameters (id, description, param_value_date) VALUES (last_docservers_size_calculation, 'Date de dernier calcul de la taille des docservers', 2024-01-01 00:00:00);
