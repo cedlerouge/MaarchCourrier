1e7f2aec4fd80c026ad6b1e52ac39f2cb8c81400
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2024-01-19 16:05:04 +0100

    FEAT #27349 TIME 0 use docItemResId in tech log

--- src/app/external/externalSignatoryBook/controllers/FastParapheurController.php
+++ src/app/external/externalSignatoryBook/controllers/FastParapheurController.php
@@ -428,7 +428,7 @@ class FastParapheurController
                         $title = $version == 'resLetterbox' ? $value['subject'] : $value['title'];
                         $documentLink->removeExternalLink($docItemResId, $title, $type, $value['external_id']);
                     } catch (Throwable $th) {
-                        $info = "[SCRIPT] Failed to remove document link: MaarchCourrier docId {$value['res_id']}, document type $type; parapheur docId {$value['external_id']}";
+                        $info = "[SCRIPT] Failed to remove document link: MaarchCourrier docId {$docItemResId}, document type $type; parapheur docId {$value['external_id']}";
                         LogsController::add([
                             'isTech'    => true,
                             'moduleId'  => $GLOBALS['moduleId'],
