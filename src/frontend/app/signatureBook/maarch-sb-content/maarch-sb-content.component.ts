edec898f069ebaead6ceb748e6db6f3e8df628f0
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2024-02-05 18:23:59 +0100

    FEAT #27733 TIME 3:30 set meta-data and document content

--- src/frontend/app/signatureBook/maarch-sb-content/maarch-sb-content.component.ts
+++ src/frontend/app/signatureBook/maarch-sb-content/maarch-sb-content.component.ts
@@ -1,7 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { Component, OnInit, Sanitizer } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActionsService } from '@appRoot/actions/actions.service';
+import { MessageActionInterface } from '@models/actions.model';
+import { Attachment, AttachmentInterface } from '@models/attachment.model';
+import { FunctionsService } from '@service/functions.service';
 import { NotificationService } from '@service/notification/notification.service';
-import { Subscription } from 'rxjs';
+import { Subscription, catchError, filter, map, of, tap } from 'rxjs';
+import { Observable } from 'tinymce';
 
 @Component({
     selector: 'app-maarch-sb-content',
@@ -9,16 +15,45 @@ import { Subscription } from 'rxjs';
     styleUrls: ['maarch-sb-content.component.scss'],
 })
 export class MaarchSbContentComponent implements OnInit {
-    
+
     subscription: Subscription;
+    attachmentSubscription: Subscription;
+
+    attachmentData: AttachmentInterface;
+
+    attachmentContent: any = null;
+
+    loading: boolean = true;
 
     constructor(
+        public functionsService: FunctionsService,
+        private http: HttpClient,
+        private sanitizer: DomSanitizer,
         private actionsService: ActionsService,
         private notificationService: NotificationService
     ) {
-        this.subscription = this.actionsService.catchAction().subscribe(message => {
-            this.notificationService.success('apposition de la griffe!');
-        });
+        this.subscription = this.actionsService.catchAction().pipe(
+            filter((data: MessageActionInterface) => data.id === 'selectedStamp'),
+            tap(() => {
+                this.notificationService.success('apposition de la griffe!');
+            }),
+            catchError((err: any) => {
+                this.notificationService.handleSoftErrors(err);
+                return of(false);
+            })
+        ).subscribe();
+
+        this.attachmentSubscription = this.actionsService.catchAction().pipe(
+            filter((data: MessageActionInterface) => data.id === 'attachmentToSign'),
+            tap((res: MessageActionInterface) => {
+                this.attachmentData = res.data;
+                this.loadContent();
+            }),
+            catchError((err: any) => {
+                this.notificationService.handleSoftErrors(err);
+                return of(false);
+            })
+        ).subscribe();
     }
 
     ngOnInit(): void {}
@@ -26,5 +61,46 @@ export class MaarchSbContentComponent implements OnInit {
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
         this.subscription.unsubscribe();
+        this.attachmentSubscription.unsubscribe();
+    }
+
+    setTitle(attachment: AttachmentInterface): string {
+        return !this.functionsService.empty(attachment?.chrono) ? `${attachment?.chrono}: ${attachment?.title}` : `${attachment?.title}`;
+    }
+
+    loadContent(): void {
+        this.loading = true;
+        this.attachmentContent = '';
+        const documentType: string = !this.functionsService.empty(this.attachmentData?.resIdMaster) ? 'attachment' : 'resource';
+        if (documentType === 'attachment') {
+            this.requestWithLoader(`../rest/attachments/${this.attachmentData.resId}/content?mode=base64`).pipe(
+                tap((data: any) => {
+                    if (data.encodedDocument) {
+                        this.attachmentContent = this.sanitizer.bypassSecurityTrustResourceUrl(`data:${data.mimeType};base64,${data.encodedDocument}`);
+                        this.loading = false;
+                    }
+                }),
+                catchError((err: any) => {
+                    this.notificationService.handleSoftErrors(err);
+                    return of(false);
+                })
+            ).subscribe();
+        }
+    }
+
+    requestWithLoader(url: string) {
+        return this.http.get<any>(url, { reportProgress: true, observe: 'events' }).pipe(
+            map((event) => {
+                switch (event.type) {
+                    case HttpEventType.DownloadProgress:
+                        const downloadProgress = Math.round(100 * event.loaded / event.total);
+                        return { status: 'progressDownload', message: downloadProgress };
+                    case HttpEventType.Response:
+                        return event.body;
+                    default:
+                        return `Unhandled event: ${event.type}`;
+                }
+            })
+        );
     }
 }
