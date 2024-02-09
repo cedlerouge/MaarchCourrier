6c1b40bbdc7bea993e5f94249bc784bbbb4a593b
Guillaume Heurtier <guillaume.heurtier@maarch.org>
2024-02-09 10:19:53 +0100

    FEAT #27897 TIME 0:10 add missing copyright in tests

--- test/Functional/Core/Error/ErrorHandlerTest.php
+++ test/Functional/Core/Error/ErrorHandlerTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief ErrorHandlerTest
+ * @author dev@maarch.org
+ */
+
 namespace MaarchCourrier\Tests\Functional\Core\Error;
 
 use MaarchCourrier\Core\Infrastructure\Error\ErrorHandler;
--- test/Functional/Core/Error/Mock/StubProblem.php
+++ test/Functional/Core/Error/Mock/StubProblem.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief StubProblem
+ * @author dev@maarch.org
+ */
+
 namespace MaarchCourrier\Tests\Functional\Core\Error\Mock;
 
 use MaarchCourrier\Core\Domain\Problem\Problem;
