ec5a854db00c7dcead0333551bf7439d274d2312
Alex ORLUC <alex.orluc@maarch.org>
2024-02-23 15:44:20 +0100

    FEAT #27675 TIME 0:20 lint file

--- src/frontend/app/actions/visa-continue-circuit-action/continue-visa-circuit-action.component.ts
+++ src/frontend/app/actions/visa-continue-circuit-action/continue-visa-circuit-action.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Inject, ViewChild, ViewContainerRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationService } from '@service/notification/notification.service';
-import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
+import {
+    MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
+    MatLegacyDialogRef as MatDialogRef,
+} from '@angular/material/legacy-dialog';
 import { HttpClient } from '@angular/common/http';
 import { NoteEditorComponent } from '../../notes/note-editor.component';
 import { tap, finalize, catchError } from 'rxjs/operators';
@@ -11,14 +14,12 @@ import { VisaWorkflowComponent } from '../../visa/visa-workflow.component';
 import { PluginManagerService } from '@service/plugin-manager.service';
 import { AuthService } from '@service/auth.service';
 import { HeaderService } from '@service/header.service';
-import { ActionsService } from '../actions.service';
 
 @Component({
     templateUrl: 'continue-visa-circuit-action.component.html',
     styleUrls: ['continue-visa-circuit-action.component.scss'],
 })
 export class ContinueVisaCircuitActionComponent implements OnInit {
-
     @ViewChild('myPlugin', { read: ViewContainerRef, static: true }) myPlugin: ViewContainerRef;
     @ViewChild('noteEditor', { static: true }) noteEditor: NoteEditorComponent;
     @ViewChild('appVisaWorkflow', { static: false }) appVisaWorkflow: VisaWorkflowComponent;
@@ -39,14 +40,12 @@ export class ContinueVisaCircuitActionComponent implements OnInit {
         public http: HttpClient,
         public dialogRef: MatDialogRef<ContinueVisaCircuitActionComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any,
-        private actionsService: ActionsService,
         private notify: NotificationService,
         public functions: FunctionsService,
         private pluginManagerService: PluginManagerService,
         private authService: AuthService,
         private headerService: HeaderService
-    ) {
-    }
+    ) {}
 
     async ngOnInit(): Promise<void> {
         this.loading = true;
@@ -61,57 +60,81 @@ export class ContinueVisaCircuitActionComponent implements OnInit {
                 resource: this.data.resource.documentToCreate,
                 sender: `${this.headerService.user.firstname} ${this.headerService.user.lastname}`,
                 externalSignatoryBookUrl: (this.authService.externalSignatoryBook.url as string).replace(/\/$/, ''),
-                externalUserId: this.headerService.user.externalId
-            }
+                externalUserId: this.headerService.user.externalId,
+            },
         };
-        this.componentInstance = await this.pluginManagerService.initPlugin('maarch-plugins-fortify', this.myPlugin, data);
+        this.componentInstance = await this.pluginManagerService.initPlugin(
+            'maarch-plugins-fortify',
+            this.myPlugin,
+            data
+        );
     }
 
     checkSignatureBook() {
         this.resourcesErrors = [];
         this.resourcesWarnings = [];
 
-        return new Promise((resolve, reject) => {
-            this.http.post('../rest/resourcesList/users/' + this.data.userId + '/groups/' + this.data.groupId + '/baskets/' + this.data.basketId + '/actions/' + this.data.action.id + '/checkContinueVisaCircuit', { resources: this.data.resIds })
-                .subscribe((data: any) => {
-                    if (!this.functions.empty(data.resourcesInformations.warning)) {
-                        this.resourcesWarnings = data.resourcesInformations.warning;
-                    }
+        return new Promise((resolve) => {
+            this.http
+                .post(
+                    '../rest/resourcesList/users/' +
+                        this.data.userId +
+                        '/groups/' +
+                        this.data.groupId +
+                        '/baskets/' +
+                        this.data.basketId +
+                        '/actions/' +
+                        this.data.action.id +
+                        '/checkContinueVisaCircuit',
+                    { resources: this.data.resIds }
+                )
+                .subscribe(
+                    (data: any) => {
+                        if (!this.functions.empty(data.resourcesInformations.warning)) {
+                            this.resourcesWarnings = data.resourcesInformations.warning;
+                        }
 
-                    if (!this.functions.empty(data.resourcesInformations.error)) {
-                        this.resourcesErrors = data.resourcesInformations.error;
-                        this.noResourceToProcess = this.resourcesErrors.length === this.data.resIds.length;
+                        if (!this.functions.empty(data.resourcesInformations.error)) {
+                            this.resourcesErrors = data.resourcesInformations.error;
+                            this.noResourceToProcess = this.resourcesErrors.length === this.data.resIds.length;
+                        }
+                        if (data.resourcesInformations.success) {
+                            data.resourcesInformations.success.forEach((value: any) => {
+                                if (value.mailing) {
+                                    this.resourcesMailing.push(value);
+                                }
+                            });
+                        }
+                        resolve(true);
+                    },
+                    (err: any) => {
+                        this.notify.handleSoftErrors(err);
+                        this.dialogRef.close();
                     }
-                    if (data.resourcesInformations.success) {
-                        data.resourcesInformations.success.forEach((value: any) => {
-                            if (value.mailing) {
-                                this.resourcesMailing.push(value);
-                            }
-                        });
-                    }
-                    resolve(true);
-                }, (err: any) => {
-                    this.notify.handleSoftErrors(err);
-                    this.dialogRef.close();
-                });
+                );
         });
     }
 
     async onSubmit() {
         this.loading = true;
-        const realResSelected: number[] = this.data.resIds.filter((resId: any) => this.resourcesErrors.map(resErr => resErr.res_id).indexOf(resId) === -1);
+        const realResSelected: number[] = this.data.resIds.filter(
+            (resId: any) => this.resourcesErrors.map((resErr) => resErr.res_id).indexOf(resId) === -1
+        );
         if (this.componentInstance?.maarchFortifyService?.signatureMode === 'rgs_2stars') {
-            this.componentInstance.open().pipe(
-                tap((data: any) => {
-                    if (!this.functions.empty(data) && typeof data === "object") {
-                        this.executeAction(realResSelected, data);
-                    }
-                }),
-                catchError((err: any) => {
-                    this.notify.handleSoftErrors(err);
-                    return of(false);
-                })
-            ).subscribe();
+            this.componentInstance
+                .open()
+                .pipe(
+                    tap((data: any) => {
+                        if (!this.functions.empty(data) && typeof data === 'object') {
+                            this.executeAction(realResSelected, data);
+                        }
+                    }),
+                    catchError((err: any) => {
+                        this.notify.handleSoftErrors(err);
+                        return of(false);
+                    })
+                )
+                .subscribe();
         } else {
             this.executeAction(realResSelected);
         }
@@ -119,22 +142,28 @@ export class ContinueVisaCircuitActionComponent implements OnInit {
     }
 
     executeAction(realResSelected: number[], objToSend: object = null) {
-
-        this.http.put(this.data.processActionRoute, { resources : realResSelected, note : this.noteEditor.getNote(), data: objToSend }).pipe(
-            tap((data: any) => {
-                if (!data) {
-                    this.dialogRef.close(realResSelected);
-                }
-                if (data && data.errors != null) {
-                    this.notify.error(data.errors);
-                }
-            }),
-            finalize(() => this.loading = false),
-            catchError((err: any) => {
-                this.notify.handleSoftErrors(err);
-                return of(false);
+        this.http
+            .put(this.data.processActionRoute, {
+                resources: realResSelected,
+                note: this.noteEditor.getNote(),
+                data: objToSend,
             })
-        ).subscribe();
+            .pipe(
+                tap((data: any) => {
+                    if (!data) {
+                        this.dialogRef.close(realResSelected);
+                    }
+                    if (data && data.errors != null) {
+                        this.notify.error(data.errors);
+                    }
+                }),
+                finalize(() => (this.loading = false)),
+                catchError((err: any) => {
+                    this.notify.handleSoftErrors(err);
+                    return of(false);
+                })
+            )
+            .subscribe();
     }
 
     isValidAction() {
--- src/frontend/app/signatureBook/content/signature-book-content.component.ts
+++ src/frontend/app/signatureBook/content/signature-book-content.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, OnDestroy, ViewChild, ViewContainerRef } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
 import { ActionsService } from '@appRoot/actions/actions.service';
 import { MessageActionInterface } from '@models/actions.model';
@@ -16,8 +16,7 @@ import { Subscription, catchError, finalize, of, tap } from 'rxjs';
     templateUrl: 'signature-book-content.component.html',
     styleUrls: ['signature-book-content.component.scss'],
 })
-export class MaarchSbContentComponent implements OnInit, OnDestroy {
-
+export class MaarchSbContentComponent implements OnDestroy {
     @ViewChild('myPlugin', { read: ViewContainerRef, static: true }) myPlugin: ViewContainerRef;
 
     subscription: Subscription;
@@ -41,34 +40,37 @@ export class MaarchSbContentComponent implements OnInit, OnDestroy {
         private notificationService: NotificationService,
         private pluginManagerService: PluginManagerService,
         private translateService: TranslateService,
-        private headerService: HeaderService,
+        private headerService: HeaderService
     ) {
-        this.subscription = this.actionsService.catchActionWithData().pipe(
-            tap(async (res: MessageActionInterface) => {
-                if (res.id === 'selectedStamp') {
-                    if (this.pluginInstance) {
-                        const signContent = await this.getSignatureContent(res.data.contentUrl);
-                        this.pluginInstance.addStamp(signContent);
+        this.subscription = this.actionsService
+            .catchActionWithData()
+            .pipe(
+                tap(async (res: MessageActionInterface) => {
+                    if (res.id === 'selectedStamp') {
+                        if (this.pluginInstance) {
+                            const signContent = await this.getSignatureContent(res.data.contentUrl);
+                            this.pluginInstance.addStamp(signContent);
+                        }
+                    } else if (res.id === 'attachmentToSign') {
+                        this.loading = true;
+                        this.subscriptionDocument?.unsubscribe();
+                        this.documentData = res.data;
+                        this.documentType = !this.functionsService.empty(this.documentData?.resIdMaster)
+                            ? 'attachments'
+                            : 'resources';
+                        setTimeout(async () => {
+                            await this.loadContent();
+                            this.initPlugin();
+                        }, 1000);
                     }
-                } else if (res.id === 'attachmentToSign') {
-                    this.loading = true;
-                    this.subscriptionDocument?.unsubscribe();
-                    this.documentData = res.data;
-                    this.documentType = !this.functionsService.empty(this.documentData?.resIdMaster) ? 'attachments' : 'resources';
-                    setTimeout(async () => {
-                        await this.loadContent();
-                        this.initPlugin();
-                    }, 1000);
-                }
-            }),
-            catchError((err: any) => {
-                this.notificationService.handleSoftErrors(err);
-                return of(false);
-            })
-        ).subscribe();
+                }),
+                catchError((err: any) => {
+                    this.notificationService.handleSoftErrors(err);
+                    return of(false);
+                })
+            )
+            .subscribe();
     }
-
-    ngOnInit(): void { }
 
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
@@ -77,23 +79,25 @@ export class MaarchSbContentComponent implements OnInit, OnDestroy {
 
     async initPlugin() {
         const data: any = {
-            file : {
-                filename : this.documentData.title,
-                content: this.documentContent
+            file: {
+                filename: this.documentData.title,
+                content: this.documentContent,
             },
             email: this.headerService.user.mail,
             currentLang: this.translateService.instant('lang.language'),
-        }
-        this.pluginInstance = await this.pluginManagerService.initPlugin('maarch-plugins-pdftron', this.myPlugin, data)
+        };
+        this.pluginInstance = await this.pluginManagerService.initPlugin('maarch-plugins-pdftron', this.myPlugin, data);
     }
 
     getLabel(): string {
-        return !this.functionsService.empty(this.documentData?.chrono) ? `${this.documentData?.chrono}: ${this.documentData?.title}` : `${this.documentData?.title}`;
+        return !this.functionsService.empty(this.documentData?.chrono)
+            ? `${this.documentData?.chrono}: ${this.documentData?.title}`
+            : `${this.documentData?.title}`;
     }
 
     getTitle(): string {
         if (this.documentType === 'attachments') {
-            return `${this.getLabel()} (${this.documentData.typeLabel})`
+            return `${this.getLabel()} (${this.documentData.typeLabel})`;
         } else if (this.documentType === 'resources') {
             return `${this.getLabel()}`;
         }
@@ -102,39 +106,41 @@ export class MaarchSbContentComponent implements OnInit, OnDestroy {
     loadContent(): Promise<boolean> {
         this.documentContent = null;
         return new Promise((resolve) => {
-            this.subscriptionDocument = this.http.get(`../rest/${this.documentType}/${this.documentData.resId}/content`, { responseType: 'blob' }).pipe(
-                tap((data: Blob) => {
-                    this.documentContent = data;
-                    const { resId, title } = this.documentData;
-                    this.actionsService.emitActionWithData(
-                        {
+            this.subscriptionDocument = this.http
+                .get(`../rest/${this.documentType}/${this.documentData.resId}/content`, { responseType: 'blob' })
+                .pipe(
+                    tap((data: Blob) => {
+                        this.documentContent = data;
+                        const { resId, title } = this.documentData;
+                        this.actionsService.emitActionWithData({
                             id: 'documentToCreate',
-                            data: { resId, title, encodedDocument: data }
-                        }
-                    );
-                }),
-                finalize(() => {
-                    this.loading = false;
-                    resolve(true);
-                }),
-                catchError((err: any) => {
-                    this.notificationService.handleSoftErrors(err);
-                    return of(false);
-                })
-            ).subscribe();
+                            data: { resId, title, encodedDocument: data },
+                        });
+                    }),
+                    finalize(() => {
+                        this.loading = false;
+                        resolve(true);
+                    }),
+                    catchError((err: any) => {
+                        this.notificationService.handleSoftErrors(err);
+                        return of(false);
+                    })
+                )
+                .subscribe();
         });
     }
 
     getSignatureContent(contentUrl: string) {
         return new Promise((resolve) => {
-            this.http.get(contentUrl, { responseType: 'blob' })
+            this.http
+                .get(contentUrl, { responseType: 'blob' })
                 .pipe(
                     tap(async (res: Blob) => {
                         resolve(await this.blobToBase64(res));
                     }),
                     catchError((err: any) => {
                         this.notificationService.handleSoftErrors(err.error.errors);
-                        resolve(false)
+                        resolve(false);
                         return of(false);
                     })
                 )
@@ -143,7 +149,7 @@ export class MaarchSbContentComponent implements OnInit, OnDestroy {
     }
 
     blobToBase64(blob: Blob) {
-        return new Promise((resolve, _) => {
+        return new Promise((resolve) => {
             const reader = new FileReader();
             reader.onloadend = () => resolve(reader.result);
             reader.readAsDataURL(blob);
