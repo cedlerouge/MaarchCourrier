d5029ece966b6b3fb613ab04ddccb4572ab22669
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2024-03-01 14:51:53 +0100

    FEAT #27855 TIME 0:20 add Authorization module for main resource

---
+++ src/backend/Authorization/Infrastructure/MainResourceAccessControlService.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief MaarchParapheurSignatureService class
+ * @author dev@maarch.org
+ */
+
+namespace MaarchCourrier\Authorization\Infrastructure;
+
+use MaarchCourrier\Core\Domain\MainResource\MainResourceAccessControlInterface;
+use MaarchCourrier\Core\Domain\User\Port\UserInterface;
+use Resource\controllers\ResController;
+
+class MainResourceAccessControlService implements MainResourceAccessControlInterface
+{
+    public function hasRightByResId(int $resId, UserInterface $user): bool
+    {
+        return ResController::hasRightByResId(['resId' => [$resId], 'userId' => $user->getId()]);
+    }
+}
---
+++ src/backend/Core/Domain/MainResource/MainResourceAccessControlInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief MaarchParapheurSignatureService class
+ * @author dev@maarch.org
+ */
+
+namespace MaarchCourrier\Core\Domain\MainResource;
+
+use MaarchCourrier\Core\Domain\User\Port\UserInterface;
+
+interface MainResourceAccessControlInterface
+{
+    /**
+     * Check if user has rights over the resource
+     *
+     * @param   int $resId Resource id
+     * @param   UserInterface $user User
+     *
+     * @return  bool
+     */
+    public function hasRightByResId(int $resId, UserInterface $user): bool;
+}
