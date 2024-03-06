b41a35f48e64905f109668d6ce5feb87f5d27e67
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2024-03-06 14:14:11 +0100

    FEAT #27855 TIME 0 add missing copyright to files

--- test/Unit/Authorization/Mock/MainResourceAccessControlServiceMock.php
+++ test/Unit/Authorization/Mock/MainResourceAccessControlServiceMock.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief MainResourceAccessControlServiceMock class
+ * @author dev@maarch.org
+ */
+
 namespace MaarchCourrier\Tests\Unit\Authorization\Mock;
 
 use MaarchCourrier\Core\Domain\MainResource\Port\MainResourceAccessControlInterface;
