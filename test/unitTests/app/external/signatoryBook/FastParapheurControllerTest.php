fbfe0807c650cc0e7328665787513a9687660f05
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2024-04-19 13:11:59 +0200

    FEAT #28613 TIME 0:20 add tests for prepareStampsStep

--- test/unitTests/app/external/signatoryBook/FastParapheurControllerTest.php
+++ test/unitTests/app/external/signatoryBook/FastParapheurControllerTest.php
@@ -15,17 +15,67 @@ use Exception;
 use ExternalSignatoryBook\controllers\FastParapheurController;
 use MaarchCourrier\Tests\CourrierTestCase;
 
+use function PHPUnit\Framework\assertArrayNotHasKey;
+
 class FastParapheurControllerTest extends CourrierTestCase
 {
     private static $remoteSignatoryBookPath = null;
     private static $defaultRemoteSignatoryBookPath = "modules/visa/xml/remoteSignatoryBooks.xml.default";
     private static $generalFileConfigOriginalXml = null;
+    private static $signStep = [];
+    private static $visaStep = [];
 
     protected function setUp(): void
     {
         self::$remoteSignatoryBookPath = "modules/visa/xml/remoteSignatoryBooks.xml";
         self::$generalFileConfigOriginalXml = file_get_contents(self::$defaultRemoteSignatoryBookPath);
         file_put_contents(self::$remoteSignatoryBookPath, self::$generalFileConfigOriginalXml);
+
+        self::$signStep = [
+            [
+                "resId"                => 100,
+                "mainDocument"         => true,
+                "externalId"           => "user1@maarch.test",
+                "sequence"             => 0,
+                "action"               => "sign",
+                "signatureMode"        => "sign",
+                "signaturePositions"   => [
+                    [
+                        "sequence"     => 0,
+                        "page"         => 1,
+                        "positionX"    => 27,
+                        "positionY"    => 70,
+                        "mainDocument" => false,
+                        "resId"        => 100
+                    ]
+                ],
+                "datePositions"        => [],
+                "externalInformations" => null
+            ]
+        ];
+
+        self::$visaStep = [
+            [
+                "resId"                => 100,
+                "mainDocument"         => true,
+                "externalId"           => "user1@maarch.test",
+                "sequence"             => 0,
+                "action"               => "visa",
+                "signatureMode"        => "visa",
+                "signaturePositions"   => [
+                    [
+                        "sequence"     => 0,
+                        "page"         => 1,
+                        "positionX"    => 27,
+                        "positionY"    => 70,
+                        "mainDocument" => false,
+                        "resId"        => 100
+                    ]
+                ],
+                "datePositions"        => [],
+                "externalInformations" => null
+            ]
+        ];
     }
 
     protected function tearDown(): void
@@ -360,7 +410,11 @@ class FastParapheurControllerTest extends CourrierTestCase
         $this->assertSame(_EXTERNAL_USER_FOUND_BUT_OPTION_OTP_DISABLE, $preparedSteps['error']);
     }
 
-    public function testGetThePreparedSteps(): void
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCanGetThePreparedSteps(): void
     {
         $this->enableFastParapheurSignatoryBook();
         $this->enableOptionOtp();
@@ -414,4 +468,147 @@ class FastParapheurControllerTest extends CourrierTestCase
         $this->assertSame("fastParapheurUserEmail", $preparedSteps[1]['type']);
         $this->assertSame("sign", $preparedSteps[1]['mode']);
     }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCannotPrepareStampsStepsWithNoStepsExpectAnEmptyArray(): void
+    {
+        $steps = [];
+
+        $preparedSteps = FastParapheurController::prepareStampsSteps($steps);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertEmpty($preparedSteps);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCannotPrepareStampsStepsWithNoSignaturePositionsInStepsExpectAnEmptyArray(): void
+    {
+        self::$signStep[0]['signaturePositions'] = [];
+
+        $preparedSteps = FastParapheurController::prepareStampsSteps(self::$signStep);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertEmpty($preparedSteps);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCanOnlyPrepareStampsStepsWithOnlyOneSignaturePositionInStepExpectAnEmptyArray(): void
+    {
+        self::$signStep[0]['signaturePositions'][] = [
+            "sequence"     => 0,
+            "page"         => 1,
+            "positionX"    => 30,
+            "positionY"    => 100,
+            "mainDocument" => false,
+            "resId"        => 100
+        ];
+
+        $preparedSteps = FastParapheurController::prepareStampsSteps(self::$signStep);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertNotEmpty($preparedSteps);
+        $this->assertArrayHasKey(100, $preparedSteps);
+        $this->assertSame(1, count($preparedSteps[100]));
+        $this->assertArrayHasKey('pictogramme-signature', $preparedSteps[100]);
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-signature']));
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-signature']));
+        $this->assertArrayNotHasKey(0, $preparedSteps[100]['pictogramme-signature'][0]['position']);
+        $this->assertSame(27, $preparedSteps[100]['pictogramme-signature'][0]['position']['x']);
+        $this->assertSame(70, $preparedSteps[100]['pictogramme-signature'][0]['position']['y']);
+    }
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function testCanPrepareSignStampsStepsWithSignaturePositionsInStepsExpectAnArray(): void
+    {
+        $preparedSteps = FastParapheurController::prepareStampsSteps(self::$signStep);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertNotEmpty($preparedSteps);
+        $this->assertArrayHasKey(100, $preparedSteps); // signable document has a stamp position
+        $this->assertSame(1, count($preparedSteps[100])); // signable document has only one type of stamp
+        $this->assertArrayHasKey('pictogramme-signature', $preparedSteps[100]); // signable document has sign stamp
+        // signable document has only one sign stamp
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-signature']));
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanPrepareStampStepsWithTwoSignableDocumentsAndOneStampPositionExpectOneDocumentToHaveOneSignStampPosition(): void
+    {
+        self::$signStep[] = [
+            "resId"                => 101,
+            "mainDocument"         => false,
+            "externalId"           => "user2@maarch.test",
+            "sequence"             => 1,
+            "action"               => "sign",
+            "signatureMode"        => "sign",
+            "signaturePositions"   => [],
+            "datePositions"        => [],
+            "externalInformations" => null
+        ];
+
+        $preparedSteps = FastParapheurController::prepareStampsSteps(self::$signStep);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertNotEmpty($preparedSteps);
+        $this->assertArrayHasKey(100, $preparedSteps);
+        $this->assertSame(1, count($preparedSteps[100]));
+        $this->assertArrayHasKey('pictogramme-signature', $preparedSteps[100]);
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-signature']));
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetVisaStampStepFromWorkflowWithUserStampPosition(): void
+    {
+        $preparedSteps = FastParapheurController::prepareStampsSteps(self::$visaStep);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertNotEmpty($preparedSteps);
+        $this->assertArrayHasKey(100, $preparedSteps);
+        $this->assertSame(1, count($preparedSteps[100]));
+        $this->assertArrayHasKey('pictogramme-visa', $preparedSteps[100]);
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-visa']));
+    }
+
+    /**
+     * @return void
+     */
+    public function testCanGetVisaAndSignStampsStepsForTheSameDocument(): void
+    {
+        self::$signStep[0]['sequence'] = 1;
+        self::$signStep[0]['signaturePositions'][0]['sequence'] = 1;
+
+        $steps = [
+            self::$visaStep[0],
+            self::$signStep[0]
+        ];
+
+        $preparedSteps = FastParapheurController::prepareStampsSteps($steps);
+
+        $this->assertIsArray($preparedSteps);
+        $this->assertNotEmpty($preparedSteps);
+        $this->assertArrayHasKey(100, $preparedSteps);
+        $this->assertSame(2, count($preparedSteps[100]));
+        $this->assertArrayHasKey('pictogramme-visa', $preparedSteps[100]);
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-visa']));
+        $this->assertArrayHasKey('pictogramme-signature', $preparedSteps[100]);
+        $this->assertSame(1, count($preparedSteps[100]['pictogramme-signature']));
+    }
+
+
 }
