b5114b1ca55cc27cfa671cc0590ff5251b04e78e
ncoutur <couture@docimsol.com>
2024-04-22 17:06:30 +0200

    FEAT #28639 TIME 00:20 Add message in history (linked to resIdMaster) if PJ is signed/refused

--- src/backend/SignatureBook/Infrastructure/SignatureHistoryService.php
+++ src/backend/SignatureBook/Infrastructure/SignatureHistoryService.php
@@ -21,24 +21,62 @@ class SignatureHistoryService implements SignatureHistoryServiceInterface
 {
     public function historySignatureValidation(int $resId, ?int $resIdMaster = null): void
     {
-        HistoryController::add([
-            'tableName' => (is_null($resIdMaster)) ? 'res_letterbox' : 'res_attachments',
-            'recordId'  => $resId,
-            'eventType' => 'SIGN',
-            'eventId'   => (is_null($resIdMaster)) ? 'resourceSign' : 'attachmentSign',
-            'info'      => _DOCUMENT_SIGNED
-        ]);
+        if (!is_null($resIdMaster)) {
+            HistoryController::add([
+                'tableName' => 'res_letterbox',
+                'recordId'  => $resIdMaster,
+                'eventType' => 'SIGN',
+                'eventId'   => 'resourceSign',
+                'info'      => _ATTACHMENT_SIGNED
+            ]);
+
+            HistoryController::add([
+                'tableName' => 'res_attachments',
+                'recordId'  => $resId,
+                'eventType' => 'SIGN',
+                'eventId'   => 'attachmentSign',
+                'info'      => _ATTACHMENT_SIGNED
+            ]);
+
+        } else {
+            HistoryController::add([
+                'tableName' => 'res_letterbox',
+                'recordId'  => $resId,
+                'eventType' => 'SIGN',
+                'eventId'   => 'resourceSign',
+                'info'      => _MAIN_RESOURCE_SIGNED
+            ]);
+        }
     }
 
     public function historySignatureRefus(int $resId, ?int $resIdMaster = null): void
     {
-        HistoryController::add([
-            'tableName' => (is_null($resIdMaster)) ? 'res_letterbox' : 'res_attachments',
-            'recordId'  => $resId,
-            'eventType' => 'SIGN',
-            'eventId'   => (is_null($resIdMaster)) ? 'resourceSign' : 'attachmentSign',
-            'info'      => 'Signature refused'
-        ]);
+        if (!is_null($resIdMaster)) {
+            HistoryController::add([
+                'tableName' => 'res_letterbox',
+                'recordId'  => $resIdMaster,
+                'eventType' => 'SIGN',
+                'eventId'   => 'resourceSign',
+                'info'      => _ATTACHMENT_SIGN_REFUSED
+            ]);
+
+            HistoryController::add([
+                'tableName' => 'res_attachments',
+                'recordId'  => $resId,
+                'eventType' => 'SIGN',
+                'eventId'   => 'attachmentSign',
+                'info'      => _ATTACHMENT_SIGN_REFUSED
+            ]);
+
+        } else {
+            HistoryController::add([
+                'tableName' => 'res_letterbox',
+                'recordId'  => $resId,
+                'eventType' => 'SIGN',
+                'eventId'   => 'resourceSign',
+                'info'      => _MAIN_RESOURCE_SIGN_REFUSED
+            ]);
+        }
     }
 
     public function historySignatureError(int $resId, ?int $resIdMaster = null): void
