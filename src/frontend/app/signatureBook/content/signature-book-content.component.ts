664bc2360c94478df2a6eb591ae749c355ea00d3
Hamza HRAMCHI <hamza.hramchi@xelians.fr>
2024-04-23 16:40:43 +0200

    FEAT #28478 TIME 1:45 check presence of documents to be signed or annex to be able to navigate + destroy plugin if data is empty

--- src/frontend/app/signatureBook/content/signature-book-content.component.ts
+++ src/frontend/app/signatureBook/content/signature-book-content.component.ts
@@ -60,10 +60,12 @@ export class MaarchSbContentComponent implements OnDestroy {
                         this.documentData = res.data.attachment;
                         this.documentType = !this.functionsService.empty(this.documentData?.resIdMaster) ? 'attachments' : 'resources';
                         setTimeout(async () => {
-                            if (this.position === 'right') {
+                            if (this.position === 'right' && !this.functionsService.empty(this.documentData)) {
                                 await this.loadContent();
                                 this.initPlugin();
                             } else {
+                                this.pluginInstance = null;
+                                this.pluginManagerService.destroyPlugin(this.myPlugin);
                                 this.loading = false;
                             }
                         }, 1000);
@@ -79,7 +81,9 @@ export class MaarchSbContentComponent implements OnDestroy {
 
     ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
+        this.documentData = null;
         this.subscription.unsubscribe();
+        this.subscriptionDocument?.unsubscribe();
     }
 
     async initPlugin() {
