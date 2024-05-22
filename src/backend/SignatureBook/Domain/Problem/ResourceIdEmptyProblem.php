f36ac6deca1877b39b32b72de878c9c24165cb65
Jean-Laurent DUZANT <jean-laurent.duzant@xelians.fr>
2024-05-22 16:54:24 +0200

    FEAT #28917 TIME 0 add missing copyright info

--- src/backend/SignatureBook/Domain/Problem/ResourceIdEmptyProblem.php
+++ src/backend/SignatureBook/Domain/Problem/ResourceIdEmptyProblem.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Copyright Maarch since 2008 under licence GPLv3.
+ * See LICENCE.txt file at the root folder for more details.
+ * This file is part of Maarch software.
+ *
+ */
+
+/**
+ * @brief ResourceIdEmptyProblem
+ * @author dev@maarch.org
+ */
+
 namespace MaarchCourrier\SignatureBook\Domain\Problem;
 
 use MaarchCourrier\Core\Domain\Problem\Problem;
